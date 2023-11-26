888924ea8267b1c938b1996af1c665698bf0505f
Mintaek2 <789rkd@naver.com>
2023-11-26 12:40:37 +0900

    exercise 1,2

---
+++ Exercise-1.ts
@@ -0,0 +1,49 @@
+// 문제
+/* export type User = unknown;
+
+export const users: unknown[] = [
+  {
+    name: "Max Mustermann",
+    age: 25,
+    occupation: "Chimney sweep",
+  },
+  {
+    name: "Kate Müller",
+    age: 23,
+    occupation: "Astronaut",
+  },
+];
+
+export function logPerson(user: unknown) {
+  console.log(` - ${user.name}, ${user.age}`);
+}
+
+console.log("Users:");
+users.forEach(logPerson); */
+
+// 해설
+export type User = {
+  name: string;
+  age: number;
+  occupation: string;
+};
+
+export const users: User[] = [
+  {
+    name: "Max Mustermann",
+    age: 25,
+    occupation: "Chimney sweep",
+  },
+  {
+    name: "Kate Müller",
+    age: 23,
+    occupation: "Astronaut",
+  },
+];
+
+export function logPerson(user: User) {
+  console.log(` - ${user.name}, ${user.age}`);
+}
+
+console.log("Users:");
+users.forEach(logPerson);
---
+++ Exercise-2.ts
@@ -0,0 +1,94 @@
+/* Type "Person" is missing, please define it and use
+    it in persons array and logPerson function in order to fix
+    all the TS errors.  
+*/
+
+// 문제
+/* interface User {
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+interface Admin {
+    name: string;
+    age: number;
+    role: string;
+}
+
+export type Person = unknown;
+
+export const persons: User[]  = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    },
+    {
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver'
+    }
+];
+
+export function logPerson(user: User) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+persons.forEach(logPerson); */
+
+
+
+// 해설
+interface User {
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+interface Admin {
+    name: string;
+    age: number;
+    role: string;
+}
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    },
+    {
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver'
+    }
+];
+
+export function logPerson(person: Person) {
+    console.log(` - ${person.name}, ${person.age}`);
+}
+
+persons.forEach(logPerson);
